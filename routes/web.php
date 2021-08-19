b890bbf10aa8f2b3a5d556995f288faba375efc4
ayu.kaneko <aiyoujinzi9@gmail.com>
2021-08-19 06:26:21 +0000

    tasklist model

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,25 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// // CRUD
+// // メッセージの個別詳細ページ表示
+// Route::get('tasklists/{id}', 'TasksController@show');
+
+// // メッセージの新規登録を処理（新規登録画面を表示するためのものではありません）
+// Route::post('tasklists', 'TasksController@store');
+
+// // メッセージの更新処理（編集画面を表示するためのものではありません）
+// Route::put('tasklists/{id}', 'TasksController@update');
+
+// // メッセージを削除
+// Route::delete('tasklists/{id}', 'TasksController@destroy');
+
+// // index: showの補助ページ
+// Route::get('tasklists', 'tasksController@index');
+
+Route::get('/', 'TasksController@index');
+
+Route::resource('tasklists', 'TasksController');
