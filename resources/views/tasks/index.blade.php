b5fe4e484e5dcde488e868e843c259d5a6b60b3e
ayu.kaneko <aiyoujinzi9@gmail.com>
2021-08-20 04:30:47 +0000

    add column

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -10,6 +10,7 @@
             <thead>
                 <tr>
                     <th>id</th>
+                      <th>タイトル</th>
                     <th>タスク</th>
                 </tr>
             </thead>
@@ -19,6 +20,7 @@
                      {{-- タスク詳細ページへのリンク --}}
                     <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
                     <td>{{ $task->content }}</td>
+                     <td>{{ $message->title }}</td>
                 </tr>
                 @endforeach
             </tbody>
